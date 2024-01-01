4aa584826d7b55670ae333d7b2eb12ef97ff3c04
Muhammad Saim <muhammadsaim494@gmail.com>
2024-01-02 00:19:22 +0500

    Print functions

---
+++ print_func/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Print statement without line breaking
+	fmt.Print("Muhammad Saim")
+
+	// Print statement with line breaking
+	fmt.Println("Muhammad Saim")
+}
